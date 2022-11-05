3a7056c2f96384b1a99c3c4b93ac7e30c70de6e3
chaanheeLEE <chanhee1519@gmail.com>
2022-11-05 22:49:56 +0900

    add logging feature

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 from calculate import *
+import logging
+
+#계산기록 남기는 로그파일 생성
+logger_cal = logging.getLogger("calculation_history")
+logger_cal.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+calculation_log = logging.FileHandler('calculation.log')
+calculation_log.setFormatter(formatter)
+logger_cal.addHandler(calculation_log)
+
+#비정살 루틴 사용 기록 남기는 로그파일 생성
+logger_abnoraml = logging.getLogger("abnoraml")
+logger_abnoraml.setLevel(logging.DEBUG)
+abnormal_log = logging.FileHandler('abnormal.log')
+abnormal_log.setFormatter(formatter)
+logger_abnoraml.addHandler(abnormal_log)
 
 print("Select operation.")
 print("1.Add")
@@ -17,15 +33,23 @@ while True:
 
         if choice == '1':
             print(num1, "+", num2, "=", add(num1, num2))
+            logger_cal.debug(f"{num1} + {num2} = {add(num1, num2)}")
 
         elif choice == '2':
             print(num1, "-", num2, "=", subtract(num1, num2))
+            logger_cal.debug(f"{num1} - {num2} = {subtract(num1, num2)}")
 
         elif choice == '3':
             print(num1, "*", num2, "=", multiply(num1, num2))
-            
+            logger_cal.debug(f"{num1} * {num2} = {multiply(num1, num2)}")
+
         elif choice =='4':
-            print(num1, "/", num2, "=", divide(num1, num2))
+            if(num2==0):
+                logger_abnoraml.warning("ZeroDivisionError")
+            else:
+                print(num1, "/", num2, "=", divide(num1, num2))
+                logger_cal.debug(f"{num1} / {num2} = {divide(num1, num2)}")
+
             
 
         # check if user wants another calculation
@@ -34,3 +58,5 @@ while True:
 
     else:
         print("Invalid Input")
+        logger_abnoraml.warning("Invalid Input")
+        

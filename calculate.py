0e31c1397678deaed5f2f0dba258e69c5561c1b8
chaanheeLEE <chanhee1519@gmail.com>
2022-11-05 16:46:11 +0900

    add quest, request functions

--- calculate.py
+++ calculate.py
@@ -15,3 +15,23 @@ def multiply(x, y):
 #Need to define divide function.
 def divide (x, y):
     return x / y
+
+# this function quest next_calculation
+def quest():
+    next_calculation = input("Let's do next calculation? (yes/no): ")
+    if next_calculation.lower() == "no":
+        request()
+    elif next_calculation.lower() == "yes":
+        return
+    else:
+        quest()
+
+# This function request next_calculation
+def request():
+    re_next_calculation = input("Are you sure? (yes/no): ")
+    if re_next_calculation.lower() =="yes":
+        exit()
+    elif re_next_calculation.lower() == "no":
+        quest()
+    else:
+        request()
